dc0644d3e4014ad4f3b02c5e21fad9cd9e6d1d7d
373jd <joshdioso7@gmail.com>
2026-08-24 21:46:58 -0400

    First
    a

---
+++ First.java
@@ -0,0 +1,10 @@
+//Joshua Dioso
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Job title: Computer Systems Analyst");
+        System.out.println("Why: Much faster than average job outlook for 2024-34 and good pay");
+        System.out.println("Median salary: $103,790");
+    }
+}
