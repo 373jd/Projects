ad62089595899797326ed03ec228a07c458e3907
373jd <joshdioso7@gmail.com>
2026-08-31 21:54:38 -0400

    Tests.java

---
+++ Tests.java
@@ -0,0 +1,16 @@
+public class Tests {
+    public static void main(String[] args) {
+
+        double score1 = 43.4;
+        double score2 = 55.6;
+        double score3 = 89.3;
+
+        double avg = (score1 + score2 + score3) / 3;
+        System.out.println("Test score 1:   " + score1);
+        System.out.println("Test score 2:   " + score2);
+        System.out.println("Test score 3:   " + score3);
+        System.out.printf("The average of 3 test scores is:  %.2f%n", avg);
+
+    }
+    
+}
